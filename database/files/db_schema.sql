ff2d484f1be11c3cfb9b9cda9d12e03302bef274
Unknown <steve.henriquet@gmail.com>
2018-03-30 12:39:34 +0200

    Ajout du changement de config
    Mysqld.conf modifier avec comme bind adress 172.18.0.22 et un utilisateur test créé avec comme adresse 172.18.0.23 donc faire test live avec ces deux adresse ^^ je vais voir pour créer un docker compose pour au moins lancé avec volume partagé le web et la db

--- database/files/db_schema.sql
+++ database/files/db_schema.sql
@@ -50,9 +50,9 @@ iswork BIT NOT NULL DEFAULT 1,
 PRIMARY KEY (id_Employe)
 ) COMMENT='this is my employe table';
 
-CREATE user 'test'@172.17.0.2 identified by 'test';
+CREATE user 'test'@172.18.0.23 identified by 'test';
 CREATE user 'moi'@'localhost' identified by 'moi';
-grant all privileges on *.* TO 'test'@172.17.0.2;
+grant all privileges on *.* TO 'test'@172.18.0.23;
 grant all privileges on *.* TO 'moi'@'localhost';
 
 flush privileges;
