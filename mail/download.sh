f6b39876dc4f45c2140d5aa1440428b640f9aaf2
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-04-20 12:49:03 +0200

    Add download shell script for mail config

---
+++ mail/download.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+curl -o setup.sh https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/dev/mail/setup.sh; chmod a+x ./setup.sh
+curl -o docker-compose.yml https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/dev/mail/docker-compose.yml;
+mkdir config;
+mkdir config/opendkim;
+mkdir config/opendkim/keys;
+mkdir config/opendkim/keys/wt1.ephec-ti.be;
+curl -o config/chksum https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/dev/mail/config/chksum;
+curl -o config/postfix-accounts.cf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/dev/mail/config/postfix-accounts.cf;
+curl -o config/opendkim/KeyTable https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/dev/mail/config/opendkim/KeyTable;
+curl -o config/opendkim/SigningTable https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/dev/mail/config/opendkim/SigningTable;
+curl -o config/opendkim/TrustedHosts https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/dev/mail/config/opendkim/TrustedHosts;
+curl -o config/opendkim/keys/wt1.ephec-ti.be/mail.private https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/dev/mail/config/opendkim/keys/wt1.ephec-ti.be/mail.private;
+curl -o config/opendkim/keys/wt1.ephec-ti.be/mail.txt https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/dev/mail/config/opendkim/keys/wt1.ephec-ti.be/mail.txt;
