66e83b0a85003f340bbe8df54e64a06eb8c49318
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-05-18 14:53:50 +0200

    Change Test branch to master in download scripts and change test to latest in docker-compose

--- voip/download.sh
+++ voip/download.sh
@@ -5,11 +5,11 @@
 curl -o docker-compose.yml https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/voip/docker-compose.yml
 mkdir config
 mkdir sounds
-curl -o config/extensions.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/Test/voip/config/extensions.conf
-curl -o config/logger.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/Test/voip/config/logger.conf
-curl -o config/sip.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/Test/voip/config/sip.conf
-curl -o config/users.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/Test/voip/config/users.conf
-curl -o config/voicemail.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/Test/voip/config/voicemail.conf
-curl -o sounds/extensions.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/Test/voip/sounds/extensions.conf
-curl -o config/iax.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/Test/voip/config/iax.conf
-curl -o config/queues.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/Test/voip/config/queues.conf
+curl -o config/extensions.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/voip/config/extensions.conf
+curl -o config/logger.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/voip/config/logger.conf
+curl -o config/sip.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/voip/config/sip.conf
+curl -o config/users.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/voip/config/users.conf
+curl -o config/voicemail.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/voip/config/voicemail.conf
+curl -o sounds/extensions.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/voip/sounds/extensions.conf
+curl -o config/iax.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/voip/config/iax.conf
+curl -o config/queues.conf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/voip/config/queues.conf
