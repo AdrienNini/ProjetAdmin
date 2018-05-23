fe90028040ea8bbd0df4e44b80bcb11576609a7f
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-05-23 14:39:21 +0200

    Add DB config file

--- docker-compose/vps-web/download.sh
+++ docker-compose/vps-web/download.sh
@@ -9,3 +9,6 @@ curl -o dns/named.conf.options https://raw.githubusercontent.com/AdrienNini/Proj
 curl -o dns/db.internal.wt1.ephec-ti.be https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/config/db.internal.wt1.ephec-ti.be;
 curl -o dns/db.wt1.ephec-ti.be https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/dns/SOA/config/db.wt1.ephec-ti.be;
 
+# DB
+mkdir db;
+curl -o db/mysqld.cnf https://raw.githubusercontent.com/AdrienNini/ProjetAdmin/master/database/config/mysqld.cnf;
